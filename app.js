a88c30632cecd4c3059e97f2a51a3c0dd7325aa1
Chris Cocuzzo <11493050+groovedrm@users.noreply.github.com>
2018-10-26 13:43:37 -0400

    first commmit of pyserve

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine','ejs');
+
+app.get('/', function(req, res) {
+	// res.send('Hello World!');
+
+	res.render('index');
+});
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000')
+});
